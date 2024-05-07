664982b413cacadec7835e755473bf7e05c8a85d
LeandroBona <leandro_bona@outlook.com>
2024-05-07 19:37:58 -0300

    atualizando

---
+++ etapas.js
@@ -0,0 +1,63 @@
+let etapas = [{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+            numero: '10001',
+            nome: 'Storm Troopper que bate a cabeça',
+            partido: 'VR - Velha República',
+            fotos: [
+                { url: '4.jpg', legenda: 'Vereador', small: false }
+            ]
+        },
+        {
+            numero: '10002',
+            nome: 'Jabba The Hutt',
+            partido: 'GCH - Grande Conselho Hutt',
+            fotos: [
+                { url: '1.png', legenda: 'Vereador', small: false }
+            ]
+        },
+        {
+            numero: '10003',
+            nome: 'Luke Skywalker',
+            partido: 'AR - Aliança Rebelde',
+            fotos: [
+                { url: '2.png', legenda: 'Vereador', small: false }
+            ]
+        },
+        {
+            numero: '10004',
+            nome: 'R2D2',
+            partido: 'AR - Aliança Rebelde',
+            fotos: [
+                { url: '3.png', legenda: 'Vereador', small: false }
+            ]
+        },
+    ]
+},
+{
+        titulo: 'PREFEITO',
+        numero: 2,
+        candidatos: [{
+            numero: '66',
+            nome: 'Darth Vader',
+            partido: 'Império',
+            vice: 'Darth Maul',
+            fotos: [
+                { url: '66_1.png', legenda: 'Prefeito'},
+                { url: '66_2.png', legenda: 'Vice-Prefeito', small:true}
+            ]
+        },
+        {
+            numero: '99',
+            nome: 'Chewbacca',
+            partido: 'AR - Aliança Rebelde',
+            vice: 'Obi-Wan Kenobi',
+            fotos: [
+                { url: '99_1.jpgs', legenda: 'Prefeito'},
+                { url: '99_2.png', legenda: 'Vice-Prefeito', small:true}
+            ]
+        },
+    ]
+}
+];
---
+++ script.js
@@ -0,0 +1,37 @@
+/*Variável de controle de interface*/
+let seuVotoPara = document.querySelector('.info-voto-titulo span')
+let cargo = document.querySelector('.info-voto-cargo')
+let descricao = document.querySelector('.info-voto-detalhe')
+let aviso = document.querySelector('.instrucoes')
+let lateral = document.querySelector('.info-imagem')
+let numeros = document.querySelector('.info-voto-numeros')
+
+/*Variável de controle de ambiente*/
+let etapaAtual = 0
+let numero = ''
+let votoBranco = false
+let votos = []
+
+function comecarEtapa(){
+
+}
+
+function atualizarInterface(){
+
+}
+
+function clicou(){
+
+}
+
+function branco(){
+
+}
+
+function corrige(){
+
+}
+
+function confirma(){
+    
+}
